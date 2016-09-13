ab4947e2014dad39a0fcb1aac3dcc189106a01fe
Oleg <admin@strongdev.net>
2016-09-13 18:49:56 +0300

    Last

--- app/src/main/java/net/strongdev/generals/MainActivity.java
+++ app/src/main/java/net/strongdev/generals/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getString(1);
+        getString(2);
     }
 }
